b8f9dd0f75b645fbe51e975566f388060dada33f
Fatumaz <vld.shuvalov.i@gmail.com>
2022-10-15 13:51:42 +0300

    Самый частотный символ Формат входных данных На вход программе подается строка текста. Текст может содержать строчные и заглавные буквы английского и русского алфавита, а также цифры.
    Формат выходных данных
    Программа должна вывести символ, который появляется наиболее часто.
    
    Примечание 1. Если таких символов несколько, следует вывести последний по порядку символ.

--- python_generation/my_string.py
+++ python_generation/my_string.py
@@ -1,6 +1,18 @@
+#1 Способ
+text = input()[::-1]
+later = max(text, key=text.count)
+print(later)
+
+#2 Способ
 text = input()
 count = 0
+letter = ''
+waste = ''
 for c in text:
-    if c.isdigit():
-        count += 1
-print(count)
+    if c not in waste:
+        waste += c
+        if text.count(c) >= count:
+            count = int(text.count(c))
+            letter = c
+
+print(letter)
