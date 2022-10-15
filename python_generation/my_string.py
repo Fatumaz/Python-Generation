93e111a669826b2b0cd358434a0bf87f526b2a12
Fatumaz <vld.shuvalov.i@gmail.com>
2022-10-15 21:44:35 +0300

    Каждый третий На вход программе подается строка текста. Напишите программу, которая удаляет из нее все символы с индексами кратными 3, то есть символы с индексами 0, 3, 6, ....

--- python_generation/my_string.py
+++ python_generation/my_string.py
@@ -1,18 +1,3 @@
-#1 Способ
-text = input()[::-1]
-later = max(text, key=text.count)
-print(later)
-
-#2 Способ
-text = input()
-count = 0
-letter = ''
-waste = ''
-for c in text:
-    if c not in waste:
-        waste += c
-        if text.count(c) >= count:
-            count = int(text.count(c))
-            letter = c
-
-print(letter)
+s = list(input())
+del s[::3]
+print(*s, sep='')
