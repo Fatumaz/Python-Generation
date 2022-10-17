e238e4010f47773cbab8d3e002053047f10bf3a3
Fatumaz <vld.shuvalov.i@gmail.com>
2022-10-17 22:37:30 +0300

    Google search - 1 На вход программе подается натуральное число nn, затем nn строк, затем еще одна строка — поисковый запрос. Напишите программу, которая выводит все введенные строки, в которых встречается поисковый запрос.

--- python_generation/my_string.py
+++ python_generation/my_string.py
@@ -1,6 +1,12 @@
-lst = []
+date = []
 for _ in range(int(input())):
-    text = input()
-    if text not in lst:
-        lst.append(text)
-print(*lst, sep='\n')
+    date.append(input())
+request = (input())
+for i in date:
+    if request.lower() in i.lower():
+        print(i)
+
+# Примемы запросов
+# язык Python появился 20 февраля 1991
+# C# - отличный язык программирования
+# Python
