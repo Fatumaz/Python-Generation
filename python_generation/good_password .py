0a69850eda814b27104375a8f1eadc40ff85adec
Fatumaz <vld.shuvalov.i@gmail.com>
2022-10-27 19:39:38 +0300

    Google search - 1 На вход программе подается натуральное число nn, затем nn строк, затем еще одна строка — поисковый запрос. Напишите программу, которая выводит все введенные строки, в которых встречается поисковый запрос.

---
+++ python_generation/good_password .py
@@ -0,0 +1,48 @@
+import string
+
+
+# объявление функции
+def is_password_good(password):
+    if len(password) < 8:
+        return 'Too Weak'
+    digits = string.digits
+    lowers = string.ascii_lowercase
+    uppers = string.ascii_uppercase
+    count = 0
+    for symbols in (digits, lowers, uppers):
+        if any(c in symbols for c in password):
+            count += 1
+            continue
+    if count == 3:
+        return 'Very Good'
+    return 'Weak' if count == 1 else 'Good'
+
+
+
+
+# считываем данные
+# txt = input()
+#
+# # вызываем функцию
+# print(is_password_good(txt))
+
+
+if __name__ == '__main__':
+    assert is_password_good('') == 'Too Weak'
+    assert is_password_good('1234567') == 'Too Weak'
+    assert is_password_good('qwertyu') == 'Too Weak'
+    assert is_password_good('QAZWSXE') == 'Too Weak'
+    assert is_password_good('QAaa1') == 'Too Weak'
+    assert is_password_good('12345678') == 'Weak'
+    assert is_password_good('123456789101112') == 'Weak'
+    assert is_password_good('qwertyurfvrgt') == 'Weak'
+    assert is_password_good('QAZWSXEDCTGB') == 'Weak'
+    assert is_password_good('1234rfvr') == 'Good'
+    assert is_password_good('1234rfvrujmi') == 'Good'
+    assert is_password_good('1234CTGB') == 'Good'
+    assert is_password_good('1234CTGBWS') == 'Good'
+    assert is_password_good('CTGBWSrfvr') == 'Good'
+    assert is_password_good('GBWSrfvr') == 'Good'
+    assert is_password_good('123GBWSrfvr') == 'Very Good'
+    assert is_password_good('123456Wr') == 'Very Good'
+    assert is_password_good('qwertyfZ1') == 'Very Good'
