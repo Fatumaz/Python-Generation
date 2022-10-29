19880575b48f5855e268b156218fce43b9773799
Fatumaz <vld.shuvalov.i@gmail.com>
2022-10-29 14:22:14 +0300

    Правильная скобочная последовательность 🌶️ Напишите функцию is_correct_bracket(text), которая принимает в качестве аргумента непустую строку text, состоящую из символов ( и ) и возвращает значение True если поступившая на вход строка является правильной скобочной последовательностью и False в противном случае.
    Примечание 1. Правильной скобочной последовательностью называется строка, состоящая только из символов ( и ), где каждой открывающей скобке найдется парная закрывающая скобка.

---
+++ python_generation/bracket_sequence.py
@@ -0,0 +1,35 @@
+# объявление функции
+def is_correct_bracket(text):
+    while text.rfind('()') >= 0:
+        text = text.replace('()', '')
+    return (len(text) == 0)
+
+
+# считываем данные
+# txt = input()
+#
+# # вызываем функцию
+# print(is_correct_bracket(txt))
+
+# Посчитать кол-во '('
+# Посчитать кол-во ')'
+# Сравнить кол-во '(' с ')'
+
+if __name__ == '__main__':
+    assert is_correct_bracket('((()))') == True
+    assert is_correct_bracket('(()())') == True
+    assert is_correct_bracket('(())()') == True
+    assert is_correct_bracket('()(())') == True
+    assert is_correct_bracket('()()()') == True
+    assert is_correct_bracket('()(())()()()(())()(()())((()))') == True
+    assert is_correct_bracket('()(())()(()())((()))()(())') == True
+    assert is_correct_bracket('())()()()(') == False
+    assert is_correct_bracket(')))(((') == False
+    assert is_correct_bracket('()(())()((())((()))()(())') == False
+    assert is_correct_bracket('()(())()(()())((()))()(()') == False
+    assert is_correct_bracket(')(())()(()())((()))()(())') == False
+    assert is_correct_bracket('())(()') == False
+    assert is_correct_bracket(')))') == False
+    assert is_correct_bracket('((((') == False
+    assert is_correct_bracket('())((((())))') == False
+
