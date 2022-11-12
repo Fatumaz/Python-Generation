948e1b0da8a60208846f44d37e664276ff5ce735
Fatumaz <vld.shuvalov.i@gmail.com>
2022-11-12 16:42:18 +0300

    Объеденены функции en_encryption и ru_encryption в одну и функции en_decoding, ru_decoding в одну

--- python_generation/numerical_guessing/caesar_cipher.py
+++ python_generation/numerical_guessing/caesar_cipher.py
@@ -1,56 +1,54 @@
-ru_encrpt = 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+# ru_encrpt = 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+ru_encrpt = 'БВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯА'
 en_encrpt = 'To be, or not to be, that is the question!'
-right_shift = 25
+right_shift = 7
 ru_dscrpt = 'Шсъцхр щмчжмщ йшм, нмтзж йшм лхшщзщг.'
 en_dscrpt = "Sgd fqzrr hr zkvzxr fqddmdq nm sgd nsgdq rhcd ne sgd edmbd."
 
-# en_dscrpt = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-# en_dscrpt = 'abcdefghijklmnopqrstuvwxyz'
 
-def rus_encryption(text, right_shift):
+def encryption(text, right_shift):
     for c in text:
-        if c.isalpha():
-            encrypted = ord(c) + right_shift  # индекс + ключ
-            if encrypted > 1103 or (
-                    c.isupper() and encrypted > 1071):  # Если индекс > "я" или "Я", то продолжить сначала алфавита
-                encrypted -= 32
-            print(chr(encrypted), end='')
-        else:
+        if not c.isalpha():
             print(c, end='')
 
+        encrypted = ord(c) + right_shift  # индекс + ключ
 
-def en_encryption(text, right_shift):
-    for c in text:
-        if c.isalpha():
-            encrypted = ord(c) + right_shift  # индекс + ключ
-            if (
-                    c.isupper() and encrypted > 90) or encrypted > 122:  # Если индекс >= "Z" или "z", то продолжить сначала алфавита
+        if 64 < ord(c) < 91 or 96 < ord(c) < 123:
+            # Если индекс >= "Z" или "z", то продолжить сначала алфавита
+            if encrypted > 122 or (c.isupper() and encrypted > 90):
                 encrypted -= 26
             print(chr(encrypted), end='')
-        else:
-            print(c, end='')
 
-
-def ru_decoding(text, right_shift):
-    for c in text:
-        if c.isalpha():
-            encrypted = ord(c) - right_shift  # индекс + ключ
-            if encrypted < 1040 or (
-                    c.islower() and encrypted < 1072):  # Если индекс > "а" или "А", то продолжить сначала алфавита
-                encrypted += 32
+        if 1039 < ord(c) < 1104:
+            # Если индекс > "я" или "Я", то продолжить сначала алфавита
+            if encrypted > 1103 or (c.isupper() and encrypted > 1071):
+                encrypted -= 32
             print(chr(encrypted), end='')
-        else:
+
+
+def decoding(text, right_shift):
+    for c in text:
+        if not c.isalpha():
             print(c, end='')
 
-def en_decoding(text, right_shift):
-    for c in text:
-        if c.isalpha():
-            encrypted = ord(c) - right_shift  # индекс + ключ
-            if encrypted <= 64 or (c.islower() and encrypted <= 96):  # Если индекс > "а" или "А", то продолжить сначала алфавита
+        encrypted = ord(c) - right_shift  # индекс + ключ
+
+        if 64 < ord(c) < 91 or 96 < ord(c) < 123:
+            if encrypted <= 64 or (c.islower() and encrypted <= 96):
                 encrypted += 26
             print(chr(encrypted), end='')
-        else:
-            print(c, end='')
+
+        if 1039 < ord(c) < 1104:
+            if encrypted < 1040 or (c.islower() and encrypted < 1072):
+                encrypted += 32
+            print(chr(encrypted), end='')
 
 
-en_decoding(en_dscrpt, right_shift)
+# encryption(ru_encrpt, right_shift)
+# print()
+decoding(ru_dscrpt, right_shift)
+
+# while right_shift < 25:
+#     right_shift += 1
+#     en_decoding(en_dscrpt, right_shift)
+#     print()
